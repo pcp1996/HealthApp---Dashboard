1c32b2b7bdf168b365f58d74a18e336ff66e9054
pcp1996 <charunipramo@gmail.com>
2020-04-23 19:22:59 +0530

    MAD Project - HealthApp Dashboard

---
+++ app/src/main/java/com/pramodha/health/data/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pramodha.health.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pramodha.health.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
